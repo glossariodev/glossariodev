fe721967d2f5d825c95a329130a08432e4800d0c
Vitor Correa <vitorbou@hotmail.com>
2023-06-21 15:03:12 -0300

    titulos de link, tamanho e espaçamento dos botões padronizados, tamanho da caixa de pesquisa e função que impede de colocar dois itens iguais na caixa de pesquisa

--- scripts/script.js
+++ scripts/script.js
@@ -1,113 +1,152 @@
 $(document).ready(function () {
-  $("#clearBtn").prop("disabled", true);
-  
-  // Método ajax para obtenção de dados JSON
-  $.getJSON('/scripts/csvjson.json', function (data) {
-    var table = $('#table_id').DataTable({
-      data: data,
-      bAutoWidth: false,
-      language: {
-        url: 'https://cdn.datatables.net/plug-ins/1.12.1/i18n/pt-BR.json',
-      },
-      columns: [
-        { 
-          data: "Termo", 
-          title: "Termo", 
-          className: "noVis", 
-          render: function(data, type, row) {
-            return `<span class="mx-auto container font-weight-bold text-center" id="termo">${data}</span>`;
-          }
+  $("input.form-control").prop("disabled", true);
+
+  // Método AJAX para obtenção de dados JSON
+  $.ajax({
+    url: './scripts/csvjson.json',
+    dataType: 'json',
+    success: function (data) {
+      var table = $('#table_id').DataTable({
+        data: data,
+        bAutoWidth: false,
+        language: {
+          url: 'https://cdn.datatables.net/plug-ins/1.12.1/i18n/pt-BR.json',
         },
-        { 
-          data: "Definição", 
-          title: "Definição", 
-          className: "noVis", 
-          render: function(data, type, row) {
-            let link1 = row.link1;
-            let link2 = row.link2;
-            return `<span class="mx-auto text-justify text-center"  id="definicao">${data}</span><br><a href="${link1}" target="_blank">${link1}</a><br><a href="${link2}" target="_blank">${link2}</a>`;
-          }
-        },
-        { 
-          data: "Dificuldade", 
-          title: "Nível", 
-          className: "noVis", 
-          render: function(data, type, row) {
-            switch (data) {
-              case "Iniciante":
-                return `<span class="text-justify text-center btn btn-outline-dark" id="nivel">Iniciante</span>`;
-              case "Intermediário":
-                return `<span class="text-center text-justify btn btn-outline-dark"  id="nivel">Intermediário</span>`;
-              case "Profissional":
-                return `<span class="text-center text-justify btn btn-outline-dark"  id="nivel">Profissional</span>`;
-              default:
-                return data;
+        columns: [
+          {
+            data: "Termo",
+            title: "Termo",
+            className: "noVis",
+            render: function (data, type, row) {
+              return `<span class="mx-auto container font-weight-bold text-center" id="termo">${data}</span>`;
+            }
+          },
+          {
+            data: "Definição",
+            title: "Definição",
+            className: "noVis",
+            render: function (data, type, row) {
+              let link1 = row.link1 ? row.link1.link : '';
+              let link2 = row.link2 ? row.link2.link : '';
+              let titulo1 = row.link1 ? row.link1.titulo : '';
+              let titulo2 = row.link2 ? row.link2.titulo : '';
+
+              return `<span class="mx-auto text-justify text-center"  id="definicao">${data}</span><br><a  href="${link1}" target="_blank">${titulo1}</a><br><a  href="${link2}" target="_blank">${titulo2}</a>`;
+            }
+          },
+          {
+            data: "Dificuldade",
+            title: "Nível",
+            className: "noVis",
+            render: function (data, type, row) {
+              switch (data) {
+                case "Iniciante":
+                  return `<span class="text-justify equalWidth text-center btn btn-outline-dark" id="nivel" data-dificuldade="Iniciante">Iniciante</span>`;
+                case "Intermediário":
+                  return `<span class="text-center equalWidth text-justify btn btn-outline-dark"  id="nivel" data-dificuldade="Intermediário">Intermediário</span>`;
+                case "Profissional":
+                  return `<span class="text-center equalWidth text-justify btn btn-outline-dark"  id="nivel" data-dificuldade="Profissional">Profissional</span>`;
+                default:
+                  return data;
+              }
+            }
+          },
+          {
+            data: "Tags",
+            title: "Categorias",
+            className: "noVis",
+            render: function (data, type, row) {
+              const categorias = data.split(",");
+              let tagsHTML = "";
+              categorias.forEach(function (categoria) {
+                tagsHTML += `<span class="text-justify equalWidth text-center btn btn-outline-dark btn-group-sm" id="categorias">${categoria}</span> `;
+              });
+              return tagsHTML;
             }
           }
-        },
-        { 
-          data: "Tags", 
-          title: "Categorias", 
-          className: "noVis", 
-          render: function(data, type, row) {
-            const categorias = data.split(",");
-            let tagsHTML = "";
-            categorias.forEach(function(categoria) {
-              tagsHTML += `<span class="text-justify text-center btn btn-outline-dark btn-group-sm" id="categorias">${categoria}</span> `;
-            });
-            return tagsHTML;
+        ]
+      });
+
+      var selectedDificuldades = new Set();
+      var selectedTags = new Set();
+
+      $('#table_id').on('click', 'span#categorias, span#nivel', function () {
+        var clickedText = $(this).text().trim();
+        var searchBox = $('.form-control, .form-control-sm');
+        var currentSearchValue = searchBox.val().trim();
+
+        // Verifica se o termo já está presente na caixa de pesquisa
+        if (!currentSearchValue.includes(clickedText)) {
+          // Verifica se a caixa de pesquisa já contém algum valor
+          if (currentSearchValue.length > 0) {
+            // Adiciona o valor clicado à caixa de pesquisa separado por vírgula
+            searchBox.val(currentSearchValue + ' ' + clickedText);
+          } else {
+            // Define o valor clicado como o valor da caixa de pesquisa
+            searchBox.val(clickedText);
           }
+
+          table.search(searchBox.val()).draw();
+          updateClearButton();
         }
-      ]
-    });
+      });
 
-    $('#table_id').on('click', 'span#categorias, span#nivel', function() {
-      var clickedText = $(this).text().trim();
-      var searchBox = $('.form-control, .form-control-sm');
-      var currentSearchValue = searchBox.val().trim();
+      $(".form-control, .form-control-sm").on("input", function () {
+        table.search($(this).val()).draw();
+        updateClearButton();
+      });
 
-      // Verifica se a caixa de pesquisa já contém algum valor
-      if (currentSearchValue.length > 0) {
-        // Adiciona o valor clicado à caixa de pesquisa separado por vírgula
-        searchBox.val(currentSearchValue + ' ' + clickedText);
-      } else {
-        // Define o valor clicado como o valor da caixa de pesquisa
-        searchBox.val(clickedText);
+      function updateClearButton() {
+        var searchBoxValue = $(".form-control, .form-control-sm").val().trim();
+        var clearButton = $("#clearBtn");
+
+        if (searchBoxValue.length > 0 || selectedDificuldades.size > 0 || selectedTags.size > 0) {
+          clearButton.removeAttr("disabled");
+        } else {
+          clearButton.prop("disabled", true);
+        }
       }
 
-      table.search(searchBox.val()).draw();
-      updateClearButton();
-    });
+      $("#cleanBtn").on("click", function () {
+        $(".form-control, .form-control-sm").val("").trigger("input");
+      });
 
-    $(".form-control").on("input", function () {
-      table.search($(this).val()).draw();
-      updateClearButton();
-    });
+      var urlParams = new URLSearchParams(window.location.search);
+      var termoPesquisa = urlParams.get('termo');
 
-    function updateClearButton() {
-      var searchBoxValue = $(".form-control").val().trim();
-      var clearButton = $("#clearBtn");
-
-      if (searchBoxValue.length > 0) {
-        clearButton.removeAttr("disabled");
-      } else {
-        clearButton.prop("disabled", true);
+      if (termoPesquisa) {
+        var conteudoDecodificado = decodeURIComponent(termoPesquisa);
+        $(".form-control, .form-control-sm").val(conteudoDecodificado).trigger("input");
       }
+
+      $('#table_id').on('click', 'span#nivel', function () {
+        var clickedDificuldade = $(this).attr('data-dificuldade');
+
+        if (selectedDificuldades.has(clickedDificuldade)) {
+          selectedDificuldades.delete(clickedDificuldade);
+        } else {
+          selectedDificuldades.add(clickedDificuldade);
+        }
+
+        updateClearButton();
+      });
+    },
+    error: function (jqxhr, textStatus, error) {
+      console.error("Falha ao ler o arquivo JSON: " + error);
+    }
+  });
+
+  $("#selectDificuldade").change(function() {
+    var selectedOptions = $(this).val(); // Obtém os valores selecionados
+
+    if (selectedOptions) {
+      var searchText = selectedOptions.join(" "); // Junta os valores selecionados separados por espaço
+      $(".form-control").val(searchText); // Define o valor na caixa de pesquisa
+    } else {
+      $(".form-control").val(""); // Limpa o valor da caixa de pesquisa
     }
 
-    $("#cleanBtn").on("click", function () {
-      $(".form-control").val("").trigger("input");
-    });
-
-    var urlParams = new URLSearchParams(window.location.search);
-    var termoPesquisa = urlParams.get('termo');
-
-    if (termoPesquisa) {
-      var conteudoDecodificado = decodeURIComponent(termoPesquisa);
-      $(".form-control").val(conteudoDecodificado).trigger("input");
-    }
-  }).fail(function (jqxhr, textStatus, error) {
-    console.error("Falha ao ler o arquivo JSON: " + error);
-  }); 
-
+    table.search($(".form-control").val()).draw(); // Executa a pesquisa na tabela
+    updateClearButton();
+  });
 });
